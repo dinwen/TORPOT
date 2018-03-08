ef6db1533fd97002d135cf5024b111fbf629b271
TheEggHealer <TheEggKiller@gmail.com>
2018-03-08 13:52:00 +0100

    Ops

--- Main/TORPOT/Game.cs
+++ Main/TORPOT/Game.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Svennebanan;
+using TORPOT.level;
 
 namespace TORPOT
 {
@@ -8,21 +10,44 @@ namespace TORPOT
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        ResourceManager resources;
+
+        public static Camera camera;
+
+        public static Level level;
+
+        public enum STATE
+        {
+            Game, Menu, Quit
+        };
+
+        public static STATE state = STATE.Game;
 
         public Game()
         {
+            resources = new ResourceManager();
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
+            IsMouseVisible = true;
             Content.RootDirectory = "Content";
+            graphics.IsFullScreen = false;
         }
 
         protected override void Initialize()
         {
             base.Initialize();
+
+            camera = new Svennebanan.Camera(GraphicsDevice.Viewport);
+            camera.Zoom = 1f;
+
+            level = new Level(resources);
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            resources.LoadContent(Content);
         }
 
         protected override void UnloadContent()
@@ -35,6 +60,11 @@ namespace TORPOT
                 Exit();
 
 
+            if(state == STATE.Game)
+            {
+                level.Update(gameTime);
+            }
+
             base.Update(gameTime);
         }
 
@@ -42,7 +72,16 @@ namespace TORPOT
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            if(state == STATE.Game)
+            {
+                spriteBatch.Begin(transformMatrix: camera.GetViewMatrix(), blendState: BlendState.NonPremultiplied, samplerState: SamplerState.PointClamp, depthStencilState: null, rasterizerState: null, effect: null, sortMode: SpriteSortMode.FrontToBack);
+                level.Draw(spriteBatch);
+            }
+
+            spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
 }
+
---
+++ Main/TORPOT/level/Level.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Svennebanan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TORPOT.level
+{
+    public class Level
+    {
+
+        public ResourceManager resourceManager;
+        public InputHandler inputHandler;
+
+        public Level(ResourceManager resources)
+        {
+            this.resourceManager = resources;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+        
+        public void Draw(SpriteBatch batch)
+        {
+
+        }
+
+    }
+}
