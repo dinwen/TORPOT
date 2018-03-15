7e3b5118ace8507e10b6b9673b5c2837766ae138
Daniel H <jomesa87@live.se>
2018-03-15 13:35:48 +0100

    HUD + Textures

---
+++ Main/TORPOT/src/utils/gui/HUD.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TORPOT.src.utils.gui
+{
+    public class HUD
+    {
+        private int x, y;
+
+        Vector2 position = new Vector2();
+
+
+        public HUD()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+
+        }
+
+
+
+
+
+    }
+}
